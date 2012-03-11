1b724352643e34fba2f2229a80776a1de81e9262
ikazoy <giboshi31@hotmail.co.jp>
2012-03-11 15:46:15 +0900

    add comment and refactoring

--- hatena_account_switch.js
+++ hatena_account_switch.js
@@ -2,35 +2,46 @@ javascript:
 (
 function(){
 
-	var logout = document.createElement('form');
-	document.body.appendChild(logout);
-	logout.setAttribute('action', 'https://www.hatena.ne.jp/logout');
-	logout.setAttribute('method', 'post');
-
+	/*
+	idとpasswordを入力させる
+	your idの部分を変えれば、デフォルトで入力されているものを変更できる
+	*/
 	var id = prompt('ID?', 'your id');
 	var pass = prompt('PASS?', '');
 
-	var form = document.createElement('form');
-	document.body.appendChild(form);
+	var login = document.createElement('form');
+	document.body.appendChild(login);
 
+	// なんかこの辺長い
 	var hatena_id = document.createElement('input');
 	hatena_id.setAttribute('type','text');
 	hatena_id.setAttribute('name', 'name');
 	hatena_id.setAttribute('id', 'login-name');
 	hatena_id.setAttribute('value', id );
-	form.appendChild(hatena_id);
+	login.appendChild(hatena_id);
 
 	var hatena_pass = document.createElement('input');
 	hatena_pass.setAttribute('type', 'password');
 	hatena_pass.setAttribute('name', 'password');
 	hatena_pass.setAttribute('class', 'password');
 	hatena_pass.setAttribute('value', pass);
-	form.appendChild(hatena_pass);
+	login.appendChild(hatena_pass);
 	
-	form.setAttribute('action', 'https://www.hatena.ne.jp/login?location=http%3A%2F%2Fb.hatena.ne.jp%2F' + id + '%2F');
-	form.setAttribute('method', 'post');
+	/*
+	 "location="以降を変更すればリダイレクト先を変更できる
+	今はとりあえず自分のブックマークページにしている
+	*/
+	login.setAttribute('action', 'https://www.hatena.ne.jp/login?location=http%3A%2F%2Fb.hatena.ne.jp%2F' + id + '%2F');
+	login.setAttribute('method', 'post');
 
+	// ログアウト処理
+	var logout = document.createElement('form');
+	document.body.appendChild(logout);
+	logout.setAttribute('action', 'https://www.hatena.ne.jp/logout');
+	logout.setAttribute('method', 'post');
 	logout.submit();
-	form.submit();
+
+	// ログイン処理	
+	login.submit();
 }
 )();
